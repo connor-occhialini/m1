034b1ae60713e991371c0498a2f0ce85bce70fba
connor-occhialini <connor.occhialini@uconn.edu>
2015-11-01 16:47:17 -0500

    second commit

--- driver.c
+++ driver.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
-#include "pi_funs.h"
-#include<math.h>
-#include "timer.c"
-#include "adjust.c"
+#include "pi_funs.h" 	/* Function declarations for series computations and M_PI */
+#include<math.h>	
+#include "timer.c"	/* Function declarations for timer_start() and timer_stop() */
+#include "adjust.c"	/* Function declaration for adjust_rep_count() */
 
 int main(void)
 {
+	/*Declare tolerance level (accuracy to which we want PI approximation)*/
 	double tol = 0.000001;
 	double abserr;
 	double pi;
-
+	
+	/*Compute Leibniz Series until pi approximation has coverged to within tol of M_PI
+	 *prints number of series terms, the approximation and the relative error at each stage
+	 *returns number of iterations necessary at the end*/
 	int i = 1;
 	printf("Leibniz Series\n");
 	do
@@ -23,6 +27,9 @@ int main(void)
 	int nleib = i/2;	
 	printf("Total iterations for Leibniz: %d\n", nleib);
 
+	/*Compute BBP Series until pi approximation has coverged to within tol of M_PI
+	 *prints number of series terms, the approximation and the relative error at each stage
+	 *returns number of iterations necessary at the end*/	
 	i = 1;
 	printf("\n Bailey-Borwein-Plouffe Series \n");
 	do
@@ -36,11 +43,17 @@ int main(void)
 	int nbbp = i/2;
 	printf("Total iterations for BBP: %d\n", nbbp);
 
+	/* Define the intial number of counts, tmin, tmax*/
 	double time, tleib, tbbp;
 	int tmin = 1;
 	int tmax = 2;
 	int count = 1000;
-
+	
+	/*Runs the leibniz series function at number of iterations for our requested
+	 *accuracy with respect to M_PI as many times as "count".  The time is recorded,
+	 *and the time for each function call is saved as tleib. The number of counts
+	 *is adjusted until the total time for count calls of pi_leibniz is between 1
+	 *1 and 2 seconds.*/
 	printf("\n Timing for Leibniz Series:\n");
 	do
 	{
@@ -56,6 +69,11 @@ int main(void)
 	}
 	while ((time >tmax) || (time <tmin));
 
+	/*Runs the BBP series function at number of iterations for our requested
+	 *accuracy with respect to M_PI as many times as "count".  The time is recorded,
+	 *and the time for each function call is saved as tbbp. The number of counts
+	 *is adjusted until the total time for count calls of pi_bbp is between 1
+	 *1 and 2 seconds.*/
 	printf("\n Timing for BBP Series:\n");
 	do
 	{
@@ -71,6 +89,8 @@ int main(void)
 	}
 	while ((time >tmax) || (time <tmin));
 
+	/*Prints time per function call for leibniz and bbp series, and then takes
+	 *the ratio between the times to compare the relative speed*/
 	printf("\nTime per function call for Leibniz:    %10.6f usec", tleib*1.e6);
 	printf("\nTime per function call for BBP:        %10.6f usec", tbbp*1.e6);
 	int ratio = tleib/tbbp;
